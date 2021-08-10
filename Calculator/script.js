dd231974a3776fd62b634588f71cd50f1e82fd5c
Urbandonment <66505497+Urbandonment@users.noreply.github.com>
2021-08-10 13:39:58 +0700

    Update Applications

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,17 +1,36 @@
 'use strict';
+let preOutput = document.getElementById('preValue');
+let newOutput = document.getElementById('newValue');
+let number = document.getElementsByClassName('number');
 
-const display = document.querySelector('.display');
-const buttons = document.querySelectorAll('button');
-
-const calculate = function (event) {
-  const clickedButtonValue = event.target.value;
-  display.value += clickedButtonValue;
-
-  if (clickedButtonValue == 'C') {
-    display.value = '';
-  }
+// Convert from Number to String to display decimal place
+const formatNum = function (num) {
+  return Number(num).toLocaleString('en');
 };
 
-buttons.forEach(function (button) {
-  button.addEventListener('click', calculate);
-});
+// Revert the format number back to Number
+const revertFormatNum = function (num) {
+  let revertNum = String(num).replace(/,/g, '');
+  return Number(revertNum);
+};
+
+// Display the previous operand
+const printPreOutput = function (num) {
+  preOutput.innerText = num;
+};
+
+// Display the current operand
+const printNewOutput = function (num) {
+  newOutput.innerText = num;
+};
+
+// Handle click action on number keys
+for (let i = 0; i < number.length; i++) {
+  number[i].addEventListener('click', function () {
+    let result = revertFormatNum(newOutput);
+    if (result != NaN) {
+      result += this.id;
+      printNewOutput(result);
+    }
+  });
+}
